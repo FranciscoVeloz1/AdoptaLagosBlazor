b645dd6854c4b3ba9548c4a12dc5f711a834a271
Francisco Veloz <franciscoveloz245@gmail.com>
2021-11-24 16:42:25 -0600

    Proyecto de Blazor

---
+++ AdoptaLagosBlazor/wwwroot/index.js
@@ -0,0 +1,23 @@
+﻿let net;
+
+async function app() {
+    try {
+        console.log('Loading mobilenet..');
+
+        // Load the model.
+        net = await mobilenet.load();
+        console.log('Successfully loaded model');
+
+        // Make a prediction through the model on our image.
+        const imgEl = document.getElementById('img');
+        console.log(imgEl)
+        const result = await net.classify(imgEl);
+
+        document.getElementById('raza').innerHTML = result[0].className;
+        console.log(result);
+    } catch (e) {
+
+    }
+}
+
+app();
---
+++ README.md
@@ -0,0 +1,2 @@
+# AdoptaLagosBlazor
+Proyecto final de adopta lagos... realizado por Daniel Esparza Diaz, Jesus Ivan Reyes Ortega y Francisco Gonzalez Veloz
